57c4bb7000089b022f18ff4cc619aa6ba6dddebe
KarolAleksandr <karol.aleksandar@gmail.com>
2019-03-19 13:41:43 +0300

    added algoritm function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let primeFactors = [];
+  for (let i = 2; i <= base; i++) {
+      let countOfInd, N, ZeroCount;
+      countOfInd = 0;
+      ZeroCount = 0;
+      if (base % i === 0) {
+          while (base % i === 0) {
+            countOfInd++;
+              base = base / i;
+          }    
+          N = number;  
+          while (N / i > 0) {
+            ZeroCount += Math.floor(N / i);
+              N = N / i;
+          }
+          primeFactors.push(ZeroCount / countOfInd);
+          primeFactors.sort((a,b) => {
+            return a-b;
+          });
+        }                
+  }
+  return Math.floor(primeFactors[0]);
 }
